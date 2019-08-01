c303fde7cd05f42a4a7b773740ccdf8cbb286596
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-01 07:03:36 +0700

    Generate angular base app

---
+++ apps/frontend/base/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'frontend-base',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/frontend/base',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};

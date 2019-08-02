fe9bb76d9e85fe398ecad0429de8513710338e6c
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-03 00:40:17 +0700

    Add import storage modules

--- apps/frontend/storage/src/app/app.browser.module.ts
+++ apps/frontend/storage/src/app/app.browser.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { BrowserStorageModule } from '@medium-stories/storage';
+
 import { AppModule } from './app.module';
-import { AppComponent } from './core/containers/app/app.component';
 import { CoreModule } from './core/core.module';
+import { AppComponent } from './core/containers/app/app.component';
 
 @NgModule({
-  imports: [AppModule, CoreModule, BrowserAnimationsModule],
+  imports: [AppModule, CoreModule, BrowserAnimationsModule, BrowserStorageModule.forRoot()],
   bootstrap: [AppComponent]
 })
 export class AppBrowserModule {}

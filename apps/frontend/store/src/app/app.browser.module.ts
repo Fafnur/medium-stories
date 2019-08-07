a181a1f6e3260c7891eed72cdb71f5a9f0a31ba6
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-07 09:18:59 +0700

    Add root store on core module

--- apps/frontend/store/src/app/app.browser.module.ts
+++ apps/frontend/store/src/app/app.browser.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { BrowserStorageModule } from '@medium-stories/storage';
 import { BrowserTranslationModule } from '@medium-stories/translation';
@@ -17,7 +18,8 @@ import { environment } from '../environments/environment';
     BrowserStorageModule.forRoot(),
     BrowserTranslationModule.forRoot({
       config: environment.translation
-    })
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument({ logOnly: environment.production }) : []
   ],
   bootstrap: [AppComponent]
 })
--- apps/frontend/store/src/app/core/core.module.ts
+++ apps/frontend/store/src/app/core/core.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { NxModule } from '@nrwl/angular';
+
+import { RootStoreModule } from '@medium-stories/store';
 
 import { coreContainers, coreRoutes } from './core.common';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(coreRoutes, { initialNavigation: 'enabled' })],
+  imports: [
+    CommonModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot(coreRoutes, { initialNavigation: 'enabled' }),
+    RootStoreModule,
+    TranslateModule
+  ],
   declarations: [...coreContainers]
 })
 export class CoreModule {}

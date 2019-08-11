bf499f4911916acaf5f0ae5a12ab4b5817c97fe8
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-11 11:38:03 +0700

    Add angular material

--- apps/frontend/theming/src/app/home/home.common.ts
+++ apps/frontend/theming/src/app/home/home.common.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { BaseLayoutComponent } from '@medium-stories/layouts';
-
 import { HomeComponent } from './containers/home/home.component';
 
 export const homeContainers: any[] = [HomeComponent];
@@ -9,12 +7,6 @@ export const homeContainers: any[] = [HomeComponent];
 export const homeRoutes: Routes = [
   {
     path: '',
-    component: BaseLayoutComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent
-      }
-    ]
+    component: HomeComponent
   }
 ];
--- apps/frontend/theming/src/app/home/home.module.ts
+++ apps/frontend/theming/src/app/home/home.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-
-import { LayoutsModule } from '@medium-stories/layouts';
 
 import { homeContainers, homeRoutes } from './home.common';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(homeRoutes), LayoutsModule, TranslateModule],
+  imports: [CommonModule, MatCardModule, RouterModule.forChild(homeRoutes), TranslateModule],
   declarations: [...homeContainers]
 })
 export class HomeModule {}

c8027f409136f5f361ec87da66f09a4fcb64c76c
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-14 08:31:54 +0700

    Generate shared library

---
+++ libs/shared/src/lib/shared.module.spec.ts
@@ -0,0 +1,15 @@
+import { async, TestBed } from '@angular/core/testing';
+
+import { SharedModule } from './shared.module';
+
+describe('SharedModule', () => {
+  beforeEach(async(() => {
+    return TestBed.configureTestingModule({
+      imports: [SharedModule]
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    expect(SharedModule).toBeTruthy();
+  });
+});

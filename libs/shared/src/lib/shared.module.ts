689104fdc4839712d6bd0b77d34de7a3c31ddaf7
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-22 10:34:22 +0700

    Add icons for side menu

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAngleRight, faBars, faCog, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleRight, faAngleUp, faBars, faCog, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { sharedComponents, sharedDirectives, sharedPipes } from './shared.common';
@@ -16,6 +16,6 @@ export class SharedModule {
     /**
      * Add an icon to the library for convenient access in other components
      */
-    this.library.addIcons(faBars, faCog, faSearch, faAngleRight);
+    this.library.addIcons(faBars, faCog, faSearch, faAngleRight, faAngleDown, faAngleUp);
   }
 }

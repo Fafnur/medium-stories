87cbd1cda5f0cc54dd97f4a4db2f8773522eca38
Aleksandr Serenko <fafnur@yandex.ru>
2019-08-27 08:37:59 +0700

    Generate footer components for layouts

---
+++ libs/layouts/src/lib/components/footer-links/footer-links.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'medium-stories-footer-links',
+  templateUrl: './footer-links.component.html',
+  styleUrls: ['./footer-links.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterLinksComponent {}

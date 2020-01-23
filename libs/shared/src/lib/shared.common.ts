04eafabf99f1f5a92008cd234a6fd8934675c61a
Aleksandr Serenko <fafnur@yandex.ru>
2020-01-23 10:06:34 +0700

    Add came case pipe

--- libs/shared/src/lib/shared.common.ts
+++ libs/shared/src/lib/shared.common.ts
@@ -1,6 +1,7 @@
 import { IconComponent } from './components/icon/icon.component';
 import { MsLetDirective } from './directives/ms-let.directive';
 import { MsRunDirective } from './directives/ms-run.directive';
+import { CamelCasePipe } from './pipes/camel-case.pipe';
 import { LanguageLabelPipe } from './pipes/language-label.pipe';
 import { LocalizePipe } from './pipes/localize.pipe';
 import { LocalizedDatePipe } from './pipes/localized-date.pipe';
@@ -10,4 +11,4 @@ export const sharedComponents: any[] = [IconComponent];
 
 export const sharedDirectives: any[] = [MsLetDirective, MsRunDirective];
 
-export const sharedPipes: any[] = [LanguageLabelPipe, LocalizePipe, LocalizedDatePipe, PathPipe];
+export const sharedPipes: any[] = [CamelCasePipe, LanguageLabelPipe, LocalizePipe, LocalizedDatePipe, PathPipe];

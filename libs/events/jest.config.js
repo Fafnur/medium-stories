84ddb802e42072ffc214e7871df2bfea3ea55212
Aleksandr Serenko <fafnur@yandex.ru>
2020-02-28 09:09:31 +0700

    Fix libs config

--- libs/events/jest.config.js
+++ libs/events/jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   name: 'events',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/libs/events',
-  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
 };

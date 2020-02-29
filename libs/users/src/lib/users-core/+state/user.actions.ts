ed2399c78a987bbdc998df0dd7cf21105358f77f
Aleksandr Serenko <fafnur@yandex.ru>
2020-02-29 09:12:42 +0700

    Add user actions

--- libs/users/src/lib/users-core/+state/user.actions.ts
+++ libs/users/src/lib/users-core/+state/user.actions.ts
@@ -1,8 +1,16 @@
 import { createAction, props } from '@ngrx/store';
-import { UserEntity } from './user.models';
 
-export const loadUser = createAction('[User] Load User');
+import { User } from '@medium-stories/entities';
+import { ActionErrorPayload, ActionPropsPayload } from '@medium-stories/store';
 
-export const loadUserSuccess = createAction('[User] Load User Success', props<{ user: UserEntity[] }>());
+import { UserLoadPayload } from '../interfaces/user.interface';
 
-export const loadUserFailure = createAction('[User] Load User Failure', props<{ error: any }>());
+export const loadUser = createAction('[User] Load User', props<ActionPropsPayload<UserLoadPayload>>());
+
+export const loadUserCancel = createAction('[User] Load User Cancel');
+
+export const loadUserRun = createAction('[User] Load User Run');
+
+export const loadUserSuccess = createAction('[User] Load User Success', props<ActionPropsPayload<User>>());
+
+export const loadUserFailure = createAction('[User] Load User Failure', props<ActionErrorPayload>());

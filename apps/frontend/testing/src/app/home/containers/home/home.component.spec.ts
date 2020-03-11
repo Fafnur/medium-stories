2a8c7d1f8b48713c1578b3630cfcab3516ba4cbe
Aleksandr Serenko <fafnur@yandex.ru>
2020-03-11 10:54:45 +0700

    Fixed old tests

--- apps/frontend/testing/src/app/home/containers/home/home.component.spec.ts
+++ apps/frontend/testing/src/app/home/containers/home/home.component.spec.ts
@@ -1,4 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockComponents, MockPipes } from 'ng-mocks';
+
+import { AuthLoginButtonComponent } from '@medium-stories/auth';
+import { UserLoadButtonComponent } from '@medium-stories/users';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +13,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent, MockComponents(UserLoadButtonComponent, AuthLoginButtonComponent), MockPipes(TranslatePipe)]
     }).compileComponents();
   }));
 

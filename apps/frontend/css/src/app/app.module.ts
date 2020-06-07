ef8ab796fdbf4a28dc1df066bebf93f1ddcc7c6a
Aleksandr Serenko <fafnur@yandex.ru>
2020-06-07 22:04:01 +0700

    Add dependencies and fixed esling config

--- apps/frontend/css/src/app/app.module.ts
+++ apps/frontend/css/src/app/app.module.ts
@@ -9,6 +9,6 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
